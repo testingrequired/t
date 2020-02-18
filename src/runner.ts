cf0ff36ee68e497d557826d1985039e0e4bc34dd
Kylee Tilley <kyleetilley@gmail.com>
2020-02-17 22:27:56 -0600

    Use promise all to wait for all test files to run

--- src/runner.ts
+++ src/runner.ts
@@ -5,20 +5,26 @@ import path from "path";
 if (isMainThread) {
   const testFilePaths = process.argv.slice(2);
 
-  testFilePaths.forEach(testFilePath => {
-    const worker = new Worker(__filename, { workerData: testFilePath });
+  Promise.all(
+    testFilePaths.map(testFilePath => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(__filename, { workerData: testFilePath });
 
-    worker.on("message", message => {
-      console.log(`${testFilePath}: message: ${message}`);
-    });
+        worker.on("message", message => {
+          resolve(`${testFilePath}: message: ${message}`);
+        });
 
-    worker.on("error", error => {
-      console.log(`${testFilePath}: error: ${error}`);
-    });
+        worker.on("error", error => {
+          reject(`${testFilePath}: error: ${error}`);
+        });
 
-    worker.on("exit", code => {
-      console.log(`${testFilePath}: exit: ${code}`);
-    });
+        worker.on("exit", code => {
+          resolve(`${testFilePath}: exit: ${code}`);
+        });
+      });
+    })
+  ).then((...value) => {
+    console.log(value);
   });
 } else {
   const suite: Suite = require(path.join(process.cwd(), workerData));
